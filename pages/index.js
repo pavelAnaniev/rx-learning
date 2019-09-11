b4fefc9e6b567589664834365aaf0b314c057822
ananiev pavel <pavel.ananiev@rrstd.ru>
2019-09-11 23:34:00 +0300

    fix

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState, useRef } from 'react';
+import Router from 'next/router';
 import { getTodoList, getTodos } from '../common/api/http';
 import { Observable, fromEvent } from 'rxjs';
 import {
@@ -23,46 +24,43 @@ const Home = () => {
 	const inputRef = useRef();
 	const [todos, setTodos] = useState([]);
 	const [value, setValue] = useState('');
+	const [filteredTodos, setFilteredTodos] = useState([]);
 	// const observable = new Observable(subscriber => {
 	// 	subscriber.fromEvent(inputRef.current);
 	// })
 	useEffect(() => {
-		getTodoList().subscribe(res => {
-			console.log(res);
+		const s1 = getTodoList().subscribe(res => {
 		});
-		getTodos().subscribe(res => {
-			console.log(res);
+		const s2 = getTodos().subscribe(res => {
 			setTodos(res);
-		});
-		console.log(inputRef.current);
-		const observable = fromEvent(inputRef.current, 'input').pipe(
-			pluck('target', 'value'),
-			debounceTime(500),
-		).subscribe(res => {
-			const filteredTodos = todos.filter(el => el.title.toLowerCase().includes(res));
-			console.log(todos.map(el => el.title), res);
-			setTodos(filteredTodos)
+			setFilteredTodos(res);
 		});
 		return () => {
-			observable.unsubscribe();
+			s1.unsubscribe();
+			s2.unsubscribe();
 		}
 	}, []);
 
 	useEffect(() => {
-		// console.log(observable);
-		// const s = observable
-		// 	.subscribe(res => {
-		// 		// setTodos(todos.filter(el => el.title.includes(res)));
-		// 	})
-		// return () => s.unsubscribe();
-	}, [value])
+		const observable = fromEvent(inputRef.current, 'input').pipe(
+			debounceTime(500),
+			pluck('target', 'value'),
+			filter(el => el.length > 2),
+		).subscribe(res => {
+			const _filteredTodos = todos.filter(el => el.title.toLowerCase().includes(res.toLowerCase()));
+			setFilteredTodos(_filteredTodos)
+		});
+		return () => {
+			observable.unsubscribe();
+		}
+	}, [todos]);
 	return <div>
 		<input ref={inputRef} value={value} onChange={e => {
 			e.preventDefault();
 			setValue(e.target.value);
 		}} />
 		<div>
-			{todos.map((el, i) => (
+			{filteredTodos.map((el, i) => (
 				<div key={i}>
 					{el.title}
 				</div>
